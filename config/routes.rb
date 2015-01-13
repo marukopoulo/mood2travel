0d8869ef070396e75fc0ebd816a64c0bbbc9505c
marukopoulo <marukopoulo@gmail.com>
2015-01-14 01:05:44 +0900

    "a"

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/upload'
-root to: "static_pages#home"
+
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 
